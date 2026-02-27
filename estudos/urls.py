e3e96ad7c694f8d1c794ee8027c6e387aff42746
kauandragues <kaua.rodrigues1509@gmail.com>
2026-02-27 17:48:10 -0300

    fix: alterações de css e html

--- estudos/urls.py
+++ estudos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path("", home),
+    path("", views.home),
+    path("materia/<slug:id>/", views.materia),
 ]
--- estudos/views.py
+++ estudos/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request=request, template_name="estudos/pages/home.html", status=200)
+    return render(request=request, template_name="estudos/paginas/home.html", status=200)
+
+
+def materia(request, id):
+    return render(
+        request=request, template_name="estudos/paginas/materia-view.html", status=200
+    )
